a8e89f7ea7041167f5ac044aea98a47c87faaadd
Голубев Матвей <golubev_m@stilsoft.ru>
2022-07-04 13:02:30 +0300

    Исправлена утечка памяти из sse subscription

--- async-graphql-sse-server/src/main.rs
+++ async-graphql-sse-server/src/main.rs
@@ -4,7 +4,7 @@ mod schema;
 use anyhow::Error;
 use async_graphql::{
     async_stream::try_stream,
-    extensions::{ApolloTracing, Tracing},
+    extensions::Tracing,
     futures_util::{
         future::{AbortHandle, Abortable},
         StreamExt,
@@ -43,24 +43,31 @@ async fn graphql_handler_query(
         return GraphQLResponse::from(schema.execute(req).await).into_response();
     }
 
-    let (abort_handle, abort_registration) = AbortHandle::new_pair();
-    let handle = tokio::spawn(async move {
-        shutdown._notified().await;
-        abort_handle.abort()
-    });
+    // if subscription
+    {
+        let (abort_handle, abort_registration) = AbortHandle::new_pair();
+        let handle = tokio::spawn(async move {
+            shutdown._notified().await;
+            abort_handle.abort()
+        });
 
-    let mut subscription = schema.execute_stream(req);
+        let mut subscription = schema.execute_stream(req);
 
-    let stream: async_graphql::async_stream::AsyncStream<Result<Event, serde_json::Error>, _> = try_stream! {
-        while let Some(response) = subscription.next().await {
-            yield Event::default().json_data(response)?;
-        }
-        handle.abort()
-    };
+        let stream: async_graphql::async_stream::AsyncStream<Result<Event, serde_json::Error>, _> = try_stream! {
+            while let Some(response) = subscription.next().await {
+                let event = Event::default().json_data(response);
+                if event.is_err() {
+                    handle.abort();
+                }
+                yield event?;
+            }
+            handle.abort()
+        };
 
-    let stream = Abortable::new(stream, abort_registration);
+        let stream = Abortable::new(stream, abort_registration);
 
-    Sse::new(stream).into_response()
+        Sse::new(stream).into_response()
+    }
 }
 
 async fn graphql_playground() -> impl IntoResponse {
